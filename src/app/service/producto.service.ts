482d0b0b10513feaae8092f2b0f590b47f68a442
pedroog304 <pedroog304@gmail.com>
2023-08-24 15:36:03 -0600

    se agrego el metodo getall en el servicio producto para consumir la apirest

---
+++ src/app/service/producto.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductoService {
+  baseUrl:string = "http://localhost:8080/api/v1";
+
+  constructor(private http:HttpClient) { }
+
+  getAll() : Observable<any>{
+    return this.http.get(this.baseUrl + "/all");
+  }
+}
